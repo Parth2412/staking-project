7bafaff6d88ed41b67fa3264c3ba18adc2bbd15b
Parth2412 <kaloliya@gmail.com>
2024-09-15 18:07:22 +0530

    added script to initialize the smart contract
    Signed-off-by: Parth2412 <kaloliya@gmail.com>

--- staking-contract/scripts/initialize.js
+++ staking-contract/scripts/initialize.js
@@ -1,6 +1,8 @@
-const STAKING_CONTRACT_ADDRESS = "0x38f8ab969ABd357748aa06537eb7256B56D89201";
-const TOKEN_CONTRACT_ADDRESS = "0x563a1c0B793413dd08d920515fAD736fd76B98D8";
+const STAKING_CONTRACT_ADDRESS = "0x8b67cf03ed8c4cf716a5760289568461498E8cE8";
+const TOKEN_CONTRACT_ADDRESS = "0x68EBc80088002C89fB46fBda7d41dAd3016410B2";
 const MULTISIG_ACCOUNT_ADDRESS = "0xF42A2674172614345963551d340B775d72C04DaA";
+const MAX_UINT256 =
+  "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 
 async function main() {
   const [deployer] = await ethers.getSigners();
@@ -11,6 +13,18 @@ async function main() {
   const stakingContract = await StakingContract.attach(
     STAKING_CONTRACT_ADDRESS
   );
+
+  const ERC20Token = await ethers.getContractFactory("StakingToken");
+  const tokenContract = await ERC20Token.attach(TOKEN_CONTRACT_ADDRESS);
+
+  console.log("Approving staking contract to spend tokens...");
+  const approveTx = await tokenContract.approve(
+    STAKING_CONTRACT_ADDRESS,
+    MAX_UINT256
+  );
+  await approveTx.wait();
+
+  console.log("Tokens approved. Proceeding to initialize staking contract...");
 
   const tx = await stakingContract.initialize(
     TOKEN_CONTRACT_ADDRESS,
